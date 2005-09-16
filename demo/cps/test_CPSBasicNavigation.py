3db24edf9ddc7b123454304518a5ca83db02eb51
bdelbosc <bdelbosc@4f77926e-26f6-0310-91bd-caad16242514>
2005-09-16 16:37:50 +0000

    more impl

--- demo/cps/test_CPSBasicNavigation.py
+++ demo/cps/test_CPSBasicNavigation.py
@@ -22,7 +22,7 @@ $Id: test_CPSBasicNavigation.py 24747 2005-08-31 10:00:13Z bdelbosc $
 import unittest
 from random import random
 from funkload.CPSTestCase import CPSTestCase
-from funkload.utils import xmlrpc_get_credential
+from funkload.utils import xmlrpc_get_credential, xmlrpc_list_credentials
 
 class CPSBasicNavigation(CPSTestCase):
     """The funktional test case.
@@ -36,60 +36,123 @@ class CPSBasicNavigation(CPSTestCase):
         self.server_url = self.conf_get('main', 'url')
         credential_host = self.conf_get('credential', 'host')
         credential_port = self.conf_getInt('credential', 'port')
-        login, password = xmlrpc_get_credential(credential_host,
-                                                credential_port,
-                                                'Ftest_Manager')
-        self.cred_manager = {'login': login,
-                             'password': password}
-        login, password = xmlrpc_get_credential(credential_host,
-                                                credential_port,
-                                                'Ftest_Member')
-        self.cred_member = {'login': login,
-                            'password': password}
-        login, password = xmlrpc_get_credential(credential_host,
-                                                credential_port,
-                                                'Ftest_Reviewer')
-        self.cred_reviewer = {'login': login,
-                              'password': password}
 
-    def test_01_create_doc(self):
-        nb_docs = self.conf_getInt('test_01_create_doc', 'nb_docs')
+        self.cred_admin = xmlrpc_get_credential(credential_host,
+                                                credential_port,
+                                                'AdminZope')
+        self.cred_manager =  xmlrpc_get_credential(credential_host,
+                                                   credential_port,
+                                                   'AdminCps')
+        self.cred_member = xmlrpc_get_credential(credential_host,
+                                                 credential_port,
+                                                 'FL_Member')
+        self.cred_reviewer = xmlrpc_get_credential(credential_host,
+                                                   credential_port,
+                                                   'FL_Reviewer')
+        self.credential_host = credential_host
+        self.credential_port = credential_port
+        self.section_title = 'FunkLoad test' # Watchout title should generate
+        self.section_id = 'funkload-test'    # this id
+        self.workspace_title = 'FunkLoad test'
+        self.workspace_id = 'funkload-test'
+
+
+    def test_00_verif_cps(self):
         server_url = self.server_url
-        login = self.cred_member['login']
-        self.cpsLogin(login, self.cred_member['password'], "member")
+        if not self.exists(server_url):
+            langs = self.conf_getList('main', 'languages')
+            manager_mail = self.conf_get('test_00_verif_cps', 'manager_mail')
+            self.cpsCreateSite(self.cred_admin[0], self.cred_admin[1],
+                               self.cred_manager[0], self.cred_manager[1],
+                               manager_mail, langs=langs,
+                               title="CPSBasicNavigation Funkload Test Site")
+
+
+    def test_05_verif_groups(self):
+        server_url = self.server_url
+        self.cpsLogin(self.cred_manager[0], self.cred_manager[1], "manager")
+        for group in ('FL_Member', 'FL_Reviewer', 'FL_Manager'):
+            self.cpsVerifyGroup(group)
+        self.cpsLogout()
+
+    def test_06_verif_users(self):
+        server_url = self.server_url
+        self.cpsLogin(self.cred_manager[0], self.cred_manager[1], "manager")
+        for group in ('FL_Member', 'FL_Reviewer', 'FL_Manager'):
+            for user_id, user_pwd in xmlrpc_list_credentials(
+                self.credential_host, self.credential_port, group):
+                self.cpsVerifyUser(user_id=user_id,
+                                   user_pwd=user_pwd,
+                                   groups=[group])
+        self.cpsLogout()
+
+
+    def test_10_create_doc(self):
+        nb_docs = self.conf_getInt('test_10_create_doc', 'nb_docs')
+        server_url = self.server_url
+        login = self.cred_member[0]
+        self.cpsLogin(login, self.cred_member[1], "member")
         for i in range(nb_docs):
             self.cpsCreateNewsItem("%s/workspaces/members/%s" %
                                    (server_url, login))
         self.cpsLogout()
 
 
-    def test_10_publish(self):
+    def test_20_verif_folders(self):
         server_url = self.server_url
-        base_url = '%s/workspaces/ftest-workspace' % server_url
+        self.cpsLogin(self.cred_manager[0], self.cred_manager[1], "manager")
+        section_url = '%s/sections/%s' % (server_url, self.section_id)
+        if not self.exists(section_url):
+            rurl = self.cpsCreateSection(
+                '%s/sections' % server_url, self.section_title,
+                'This is a FunkLoad test section, see '
+                'http://svn.nuxeo.org/pub/funkload/trunk/README.txt '
+                'for more information.')
+            url = '%s/%s' % (server_url, rurl)
+            self.cpsSetLocalRole(url, 'group:FL_Manager', 'SectionManager')
+            self.cpsSetLocalRole(url, 'group:FL_Reviewer', 'SectionReviewer')
+            self.cpsSetLocalRole(url, 'group:FL_Member', 'SectionReader')
+        workspace_url = '%s/workspaces/%s' % (server_url, self.workspace_id)
+        if not self.exists(workspace_url):
+            rurl = self.cpsCreateWorkspace(
+                '%s/workspaces' % server_url, self.workspace_title,
+                'This is a FunkLoad test workspace, see '
+                'http://svn.nuxeo.org/pub/funkload/trunk/README.txt '
+                'for more information.')
+            url = '%s/%s' % (server_url, rurl)
+            self.cpsSetLocalRole(url, 'group:FL_Manager', 'WorkspaceManager')
+            self.cpsSetLocalRole(url, 'group:FL_Member', 'WorkspaceMember')
+        self.cpsLogout()
+
+
+    def test_22_publish(self):
+        server_url = self.server_url
+        base_url = '%s/workspaces/%s' % (server_url, self.workspace_id)
         # begin of ftest ---------------------------------------------
 
         # login in as member -----------------------------------------
-        self.cpsLogin(self.cred_member['login'], self.cred_member['password'],
+        self.cpsLogin(self.cred_member[0], self.cred_member[1],
                       "member")
         # access ftest ws
         self.get("%s/folder_contents" % base_url,
                  description="Folder contents")
-        params = [["theme", "default"],
-                  ["page", "1713069120"]]
-        self.get("%s/cpsskins_renderJS" % base_url,
-                 description="CPSSkin js")
 
         # create a news
-        doc_url, doc_id = self.cpsCreateNewsItem(base_url)
+        doc_rurl, doc_id = self.cpsCreateNewsItem(base_url)
 
         # pubish a doc
+        doc_url = server_url + '/' + doc_rurl
         self.get("%s/content_submit_form" % doc_url,
                  description="View submit form")
-        params = [["submit", "sections/ftest-section"],
+        params = [["submit", "sections/%s" % self.section_id],
                   ["comments", "ftest submit"],
                   ["workflow_action", "copy_submit"]]
         self.post("%s/content_status_modify" % doc_url, params,
                   description="Submit the document")
+        self.assert_(self.getLastUrl().find('psm_status_changed') != -1,
+                     'Failed to publish %s into sections/%s' % (
+            doc_rurl, self.section_id))
+
         # we sould see only one doc_id
         #self.assertEquals(len(self.cpsSearchDocId(doc_id)), 1,
         #"should see only one doc_id [%s]." % doc_id)
@@ -97,19 +160,21 @@ class CPSBasicNavigation(CPSTestCase):
 
 
         # login as reviewer ---------------------------------------
-        self.cpsLogin(self.cred_reviewer['login'],
-                      self.cred_reviewer['password'],
+        self.cpsLogin(self.cred_reviewer[0],
+                      self.cred_reviewer[1],
                       "reviewer")
 
         # accept the doc
-        base_url2 = "%s/sections/ftest-section" % server_url
+        base_url2 = "%s/sections/%s" % (server_url, self.section_id)
         doc_publish_url = "%s/%s" % (base_url2, doc_id)
         self.get("%s/content_accept_form" % doc_publish_url,
                  description="View the accept form")
-        params = [["comments", "nice ftest work"],
+        params = [["comments", "Nice work %s" % self.cred_member[0]],
                   ["workflow_action", "accept"]]
         self.post("%s/content_status_modify" % doc_publish_url, params,
                   description="Accept the document")
+        self.assert_(self.getLastUrl().find('psm_status_changed') != -1,
+                     'Failed to accept %s' % doc_publish_url)
 
         # check that we have 2 doc_ids
         #self.assertEquals(len(self.cpsSearchDocId(doc_id)), 2,
@@ -119,7 +184,7 @@ class CPSBasicNavigation(CPSTestCase):
         return self.steps
 
 
-    def test_20_reader(self):
+    def test_30_reader(self):
         server_url = self.server_url
 
         # home page
@@ -133,15 +198,14 @@ class CPSBasicNavigation(CPSTestCase):
                  description="Login page")
 
         # login in as member
-        self.cpsLogin(self.cred_member['login'],
-                      self.cred_member['password'],
+        self.cpsLogin(self.cred_member[0], self.cred_member[1],
                       "member")
 
         # home page
         self.get("%s/" % server_url, description="Home page logged")
 
         # home page in different languages
-        for lang in self.conf_getList('test_20_reader', 'languages'):
+        for lang in self.conf_getList('main', 'languages'):
             self.post("%s/cpsportlet_change_language" % server_url,
                       params=[['lang', lang]],
                       description="Home page in %s" % lang,
@@ -172,12 +236,12 @@ class CPSBasicNavigation(CPSTestCase):
                  description="View section Atom flux")
 
         # section
-        self.get("%s/sections/ftest-section" % server_url,
+        self.get("%s/sections/%s" % (server_url, self.section_id),
                  description="View the main section")
 
         # extract a doc link
-        my_docs = self.listDocumentHref('/sections/ftest-section/test-')
-        self.assert_(my_docs, "no doc found in ftest-section")
+        my_docs = self.listDocumentHref('/sections/%s/test-' % self.section_id)
+        self.assert_(my_docs, "no doc found in %s" % self.section_id)
         a_doc = my_docs[int(len(my_docs) * random())]
 
         self.get("%s%s" % (server_url, a_doc),
@@ -210,6 +274,13 @@ class CPSBasicNavigation(CPSTestCase):
 
         self.cpsLogout()
 
+
+
+    def test_50_reader(self):
+        server_url = self.server_url
+        # home page
+        self.get('%s' % server_url,
+                 description='Home page anonymous')
 
 
     def tearDown(self):
