54e536c9b010ad69488733597b4f2b7b2aec0a82
bdelbosc <bdelbosc@4f77926e-26f6-0310-91bd-caad16242514>
2005-09-19 13:51:14 +0000

    exists support description param

--- src/FunkLoadTestCase.py
+++ src/FunkLoadTestCase.py
@@ -314,9 +314,9 @@ class FunkLoadTestCase(unittest.TestCase):
         response = self.browse(url, params, description, code, method="get")
         return response
 
-    def exists(self, url, params=None):
+    def exists(self, url, params=None, description="Checking existance"):
         """Try a GET on URL return True if the page exists or False."""
-        resp = self.get(url, params, description="Checking existance",
+        resp = self.get(url, params, description=description,
                         code=[200, 301, 302, 404, 503])
         if resp.code not in [200, 301, 302]:
             self.logd('Page %s not found.' % url)
