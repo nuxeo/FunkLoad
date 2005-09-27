33c2836f6302692ec35cb3e0a9aab8bae4e0c3fa
bdelbosc <bdelbosc@4f77926e-26f6-0310-91bd-caad16242514>
2005-09-27 14:29:17 +0000

    udpate description

--- setup.py
+++ setup.py
@@ -26,19 +26,29 @@ from distutils.core import setup
 from src.version import __version__
 
 setup(
-    name = "funkload",
-    version = __version__,
-    description = "Functional and load web tester.",
-    long_description = """FunkLoad is a functional and load web tester.
+    name="funkload",
+    version=__version__,
+    description="Functional and load web tester.",
+    long_description="""\
+FunkLoad is a functional and load web tester, main use cases are:
+
+* Functional regression testing of web projects.
+
+* Benching web application and producing detail reports.
+
+* Writing web agents, like checking if a site is alive, or scripting any web
+  repetitive task.
+
 
 Main FunkLoad features are:
 
-* Compatible with pyUnit framework, just use funkload.FunkLoadTestCase
-  instead of unittest.TestCase.
+* Pure python package compatible with pyUnit framework, just use
+  funkload.FunkLoadTestCase instead of unittest.TestCase.
 
-* Truly emulate a web browser (single-threaded) using Richard Jones' webunit:
+* Truly emulate a web browser (single-threaded) using Richard Jones'
+  webunit:
 
-  - basic auth support
+  - basic authentication support
   - cookies support
   - fetching css, javascript and images
   - emulate a browser cache
@@ -47,8 +57,8 @@ Main FunkLoad features are:
 
 * Advanced test runner with many command line options:
 
-  - color mode
-  - display the page fetched in real time in your browser
+  - green/red color mode
+  - display the fetched page in real time in your browser
   - debug mode
 
 * Turn a functional test into a load test, just by invoking the bench runner
@@ -61,31 +71,32 @@ Main FunkLoad features are:
   - servers cpu usage, load average, memory/swap usage and network traffic
     charts.
 
-* Easy test customization using configuration file or command line.
+* Easy test customization using configuration file or command line options.
 
-* Easy test creation using TestMaker recorder, you can use your web browser
-  and produce a FunkLoad test automatically.
+* Easy test creation using TestMaker / maxq recorder, you can use your web
+  browser and produce a FunkLoad test automatically.
 
-* Web assertion helpers.
+* Provide web assertion helpers.
 
-* Provide a funkload.CPSTestCase class to ease nuxeo CPS testing.
+* Provide a funkload.CPSTestCase to ease Zope and Nuxeo CPS testing.
+
+* Easy to use, see examples in the demo folder.
 """,
-    author = "Benoit Delbosc",
-    author_email = "bdelbosc@nuxeo.com",
-    url = "http://public.dev.nuxeo.com/~ben/funkload/",
-    download_url = "http://public.dev.nuxeo.com/~ben/funkload-%s.tar.gz"
-    %__version__,
+    author="Benoit Delbosc",
+    author_email="bdelbosc@nuxeo.com",
+    url="http://public.dev.nuxeo.com/~ben/funkload/",
+    download_url="http://public.dev.nuxeo.com/~ben/funkload-%s.tar.gz"%__version__,
     license='GPL',
-    packages = ['funkload'],
-    package_dir = {'funkload': 'src'},
-    data_files = [('funkload', ['data/funkload.css',
+    packages=['funkload'],
+    package_dir={'funkload': 'src'},
+    data_files=[('funkload', ['data/funkload.css',
                                 'data/ScriptTestCase.tpl',
                                 'data/ConfigurationTestCase.tpl'])],
-    scripts = ['scripts/fl-monitor-ctl', 'scripts/fl-credential-ctl',
+    scripts=['scripts/fl-monitor-ctl', 'scripts/fl-credential-ctl',
                'scripts/fl-run-bench', 'scripts/fl-run-test',
                'scripts/fl-build-report',
                'scripts/fl-import-from-tm-recorder'],
-    classifiers = [
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
