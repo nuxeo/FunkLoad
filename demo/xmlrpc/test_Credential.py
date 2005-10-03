8cdd9fccb1ea463731d4051f54dd1999b223f7c9
bdelbosc <bdelbosc@4f77926e-26f6-0310-91bd-caad16242514>
2005-10-03 13:06:23 +0000

    Creation

---
+++ demo/xmlrpc/test_Credential.py
@@ -0,0 +1,52 @@
+# -*- coding: iso-8859-15 -*-
+"""Simple FunkLoad test
+
+$Id$
+"""
+import unittest
+from random import random
+from funkload.FunkLoadTestCase import FunkLoadTestCase
+
+class Credential(FunkLoadTestCase):
+    """This test use a configuration file Credential.conf."""
+
+    def setUp(self):
+        """Setting up test."""
+        self.logd("setUp")
+        self.server_url = self.conf_get('main', 'url')
+
+    def test_credential(self):
+        server_url = self.server_url
+        ret = self.xmlrpc_call(server_url, 'getStatus',
+                               description="Check getStatus")
+        self.assert_('Server running' in ret, 'Server is down %s' % ret)
+        self.logd('ret %s' % ret)
+        ret = self.xmlrpc_call(server_url, 'reloadConf',
+                               description="Reload configuration file")
+        self.logd('ret %s' % ret)
+        ret = self.xmlrpc_call(server_url, 'getRandomCredential',
+                               description="Get a random credential")
+        self.assertEquals(len(ret), 2, 'Invalid return %s' % ret)
+        self.logd('ret %s' % ret)
+        ret = self.xmlrpc_call(server_url, 'getFileCredential',
+                               description="Get a credential from a file")
+        self.assertEquals(len(ret), 2, 'Invalid return %s' % ret)
+        self.logd('ret %s' % ret)
+        ret = self.xmlrpc_call(server_url, 'listCredentials',
+                               description="list all credential of the file")
+        self.logd('ret %s' % ret)
+        ret = self.xmlrpc_call(server_url, 'listGroups',
+                               description="list groups from the group file")
+        self.logd('ret %s' % ret)
+        ret = self.xmlrpc_call(server_url, 'listGroups',
+                               description="list groups from the group file")
+        self.logd('ret %s' % ret)
+
+
+    def tearDown(self):
+        """Setting up test."""
+        self.logd("tearDown.\n")
+
+
+if __name__ in ('main', '__main__'):
+    unittest.main()
