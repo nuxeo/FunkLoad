4c3160daddd879f8213963e2d69c8f35798f19c9
bdelbosc <bdelbosc@4f77926e-26f6-0310-91bd-caad16242514>
2005-10-11 14:22:10 +0000

    adding create_daemon function

--- src/utils.py
+++ src/utils.py
@@ -66,6 +66,45 @@ def set_running_flag(value):
     global g_running
     g_running = value
 
+# ------------------------------------------------------------
+# daemon
+#
+# See the Chad J. Schroeder example for a full explanation
+# this version does not chdir to '/' to keep relative path
+def create_daemon():
+    """Detach a process from the controlling terminal and run it in the
+    background as a daemon.
+    """
+    try:
+        pid = os.fork()
+    except OSError, msg:
+        raise Exception, "%s [%d]" % (msg.strerror, msg.errno)
+    if (pid == 0):
+        os.setsid()
+        try:
+            pid = os.fork()
+        except OSError, msg:
+            raise Exception, "%s [%d]" % (msg.strerror, msg.errno)
+        if (pid == 0):
+            os.umask(0)
+        else:
+            os._exit(0)
+    else:
+        os._exit(0)
+    import resource
+    maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+    if (maxfd == resource.RLIM_INFINITY):
+        maxfd = 1024
+    for fd in range(0, maxfd):
+        try:
+            os.close(fd)
+        except OSError:
+            pass
+    os.open('/dev/null', os.O_RDWR)
+    os.dup2(0, 1)
+    os.dup2(0, 2)
+    return(0)
+
 
 # ------------------------------------------------------------
 # meta method name encodage
