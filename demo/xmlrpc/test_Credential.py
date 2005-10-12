6570666ff93c23f5e1b57270d1d6ac72dfdbb846
bdelbosc <bdelbosc@4f77926e-26f6-0310-91bd-caad16242514>
2005-10-12 13:54:18 +0000

    adapt to new credential server

--- demo/xmlrpc/test_Credential.py
+++ demo/xmlrpc/test_Credential.py
@@ -19,25 +19,28 @@ class Credential(FunkLoadTestCase):
         server_url = self.server_url
         ret = self.xmlrpc_call(server_url, 'getStatus',
                                description="Check getStatus")
-        self.assert_('Server running' in ret, 'Server is down %s' % ret)
+        self.assert_('running' in ret, 'Server is down %s' % ret)
         self.logd('ret %s' % ret)
-        ret = self.xmlrpc_call(server_url, 'reloadConf',
-                               description="Reload configuration file")
-        self.logd('ret %s' % ret)
-        ret = self.xmlrpc_call(server_url, 'getRandomCredential',
-                               description="Get a random credential")
-        self.assertEquals(len(ret), 2, 'Invalid return %s' % ret)
-        self.logd('ret %s' % ret)
-        ret = self.xmlrpc_call(server_url, 'getFileCredential',
+
+        ret = self.xmlrpc_call(server_url, 'getCredential',
                                description="Get a credential from a file")
+        self.logd('ret %s' % ret)
         self.assertEquals(len(ret), 2, 'Invalid return %s' % ret)
-        self.logd('ret %s' % ret)
-        ret = self.xmlrpc_call(server_url, 'listCredentials',
-                               description="list all credential of the file")
-        self.logd('ret %s' % ret)
+
         ret = self.xmlrpc_call(server_url, 'listGroups',
                                description="list groups from the group file")
         self.logd('ret %s' % ret)
+        a_group = ret[0]
+
+        ret = self.xmlrpc_call(server_url, 'listCredentials',
+                               description="list all credential of the file")
+        self.logd('ret %s' % ret)
+
+        ret = self.xmlrpc_call(server_url, 'listCredentials', (a_group,),
+                               description="list credentials of group " +
+                               a_group)
+        self.logd('ret %s' % ret)
+
 
     def tearDown(self):
         """Setting up test."""
