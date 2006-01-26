0a5578a921bb06cc0eeabf6f2c9cc6b7228db00f
bdelbosc <bdelbosc@4f77926e-26f6-0310-91bd-caad16242514>
2006-01-26 13:22:47 +0000

    spliting doctest test, add a warning in requires check

--- funkload/tests/test_Install.py
+++ funkload/tests/test_Install.py
@@ -39,7 +39,7 @@ class TestInstall(unittest.TestCase):
                       (cmd, ret[0], expected_code, ret[1]))
         return ret[1]
 
-    def test_01_checkImport(self):
+    def test_01_checkRequires(self):
         try:
             import webunit
         except ImportError:
@@ -59,6 +59,11 @@ class TestInstall(unittest.TestCase):
             print ("WARNING: missing gdchart module, "
                    "no charts available in the HTML report.")
 
+        from funkload.TestRunner import g_has_doctest
+        if not g_has_doctest:
+            print "WARNING: Python 2.4 is required to support doctest"
+
+
     def test_testloader(self):
         # check testrunner loader
         test_file = self.test_file
@@ -67,29 +72,18 @@ class TestInstall(unittest.TestCase):
         self.assert_('test_dummy1_1' in output)
         self.assert_('test_dummy2_1' in output)
         self.assert_('test_dummy3_1' in output)
-        self.assert_('Dummy.double' in output, 'missing doctest')
 
         # list a test suite
         output = self.system("fl-run-test %s test_suite --list" % test_file)
         self.assert_('test_dummy1_1' in output)
         self.assert_('test_dummy2_1' in output)
         self.assert_('test_dummy3_1' not in output)
-        self.assert_('Dummy.double' not in output,
-                     'doctest is not part of the suite')
 
         # list all test in a test case class
         output = self.system("fl-run-test %s TestDummy1 --list" % test_file)
         self.assert_('test_dummy1_1' in output)
+        self.assert_('test_dummy1_2' in output)
         self.assert_('test_dummy2_1' not in output)
-        self.assert_('Dummy.double' not in output,
-                     'doctest is not part of the testcase')
-
-        # pure doctest
-        doctest_file = self.doctest_file
-        output = self.system("fl-run-test %s --list" % doctest_file)
-        self.assert_(doctest_file.replace('.', '_') in output,
-                     'no %s in output %s' % (doctest_file, output))
-        self.assert_('test_dummy1_1' not in output)
 
         # match regex
         output = self.system("fl-run-test %s --list -e dummy1_1" % test_file)
@@ -101,29 +95,48 @@ class TestInstall(unittest.TestCase):
         self.assert_('test_dummy1_1' in output)
         self.assert_('test_dummy2_1' not in output)
 
-        output = self.system("fl-run-test %s --list -e dummy._1" % test_file)
-        self.assert_('test_dummy1_1' in output)
-        self.assert_('test_dummy2_1' in output)
-        self.assert_('test_dummy2_2' not in output)
-
         output = self.system("fl-run-test %s --list -e 2$" % test_file)
         self.assert_('test_dummy1_2' in output)
-        self.assert_('test_dummy1_1' not in output)
-
-        output = self.system("fl-run-test %s --list -e double" % test_file)
-        self.assert_('Dummy.double' in output)
-        self.assert_('test_dummy1_1' not in output)
-
-        output = self.system("fl-run-test %s --list -e double" % test_file)
-        self.assert_('Dummy.double' in output)
-        self.assert_('test_dummy1_1' not in output)
-
-        output = self.system("fl-run-test %s --list -e '!double'" % test_file)
-        self.assert_('double' not in output)
-        self.assert_('test_dummy1_1' in output)
         self.assert_('test_dummy2_2' in output)
+        self.assert_('test_dummy1_1' not in output)
+        self.assert_('test_dummy2_1' not in output)
+
+        output = self.system("fl-run-test %s --list -e '!2$'" % test_file)
+        self.assert_('test_dummy1_1' in output, output)
+        self.assert_('test_dummy2_1' in output)
+        self.assert_('test_dummy1_2' not in output)
+        self.assert_('test_dummy2_2' not in output)
 
 
+    def test_doctestloader(self):
+        # check testrunner loader
+        from funkload.TestRunner import g_has_doctest
+        if not g_has_doctest:
+            self.fail('Python 2.4 is required to support doctest')
+
+        test_file = self.test_file
+        # listing test
+        output = self.system("fl-run-test %s --list" % test_file)
+        self.assert_('Dummy.double' in output, 'missing doctest')
+
+        # list a test suite
+        output = self.system("fl-run-test %s test_suite --list" % test_file)
+        self.assert_('Dummy.double' not in output,
+                     'doctest is not part of the suite')
+
+        # list all test in a test case class
+        output = self.system("fl-run-test %s TestDummy1 --list" % test_file)
+        self.assert_('Dummy.double' not in output,
+                     'doctest is not part of the testcase')
+
+        # pure doctest
+        doctest_file = self.doctest_file
+        output = self.system("fl-run-test %s --list" % doctest_file)
+        self.assert_(doctest_file.replace('.', '_') in output,
+                     'no %s in output %s' % (doctest_file, output))
+
+        # match regex
+        output = self.system("fl-run-test %s --list -e dummy1_1" % test_file)
 
 
     def test_testrunner(self):
@@ -137,9 +150,11 @@ class TestInstall(unittest.TestCase):
         self.assert_('Ran 0 tests' not in output,
                      'not expected output:"""%s"""' % output)
         # doctest
-        output = self.system('fl-run-test %s -e double -v' % test_file)
-        self.assert_('Ran 0 tests' not in output,
-                     'not expected output:"""%s"""' % output)
+        from funkload.TestRunner import g_has_doctest
+        if g_has_doctest:
+            output = self.system('fl-run-test %s -e double -v' % test_file)
+            self.assert_('Ran 0 tests' not in output,
+                         'not expected output:"""%s"""' % output)
 
         # failing test
         output = self.system('fl-run-test %s TestDummy3 -v' % test_file,
