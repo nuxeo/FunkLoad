05571df9c00d9aa7236e7bda3211012343da715b
bdelbosc <bdelbosc@nuxeo.com>
2011-02-21 09:25:08 +0100

    Move master version to 1.14.1

--- setup.py
+++ setup.py
@@ -20,12 +20,11 @@
 #
 """FunkLoad package setup
 
-$Id: setup.py 24768 2005-08-31 14:01:05Z bdelbosc $
 """
 import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup, find_packages
-__version__ = '1.14.0'
+__version__ = '1.14.1'
 
 setup(
     name="funkload",
