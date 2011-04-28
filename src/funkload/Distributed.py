6cf5a40444cb60ffd09a5fd9f015d0fd26b64b63
Andrew McFague <amcfague@wgen.net>
2011-04-27 23:41:57 -0400

    Adding ability to monitor results from the central host in distributed mode

--- src/funkload/Distributed.py
+++ src/funkload/Distributed.py
@@ -1,13 +1,22 @@
 import os
+import platform
 import re
 import socket
 import threading
+import time
 from datetime import datetime
+from socket import error as SocketError
 from stat import S_ISREG, S_ISDIR
+from glob import glob
+from socket import error as SocketError
+from stat import S_ISREG, S_ISDIR
+from xml.etree.ElementTree import ElementTree
+from xmlrpclib import ServerProxy
 
 import paramiko
 
-from utils import mmn_encode, trace, package_tests, get_virtualenv_script
+from utils import mmn_encode, trace, package_tests, get_virtualenv_script, \
+                  get_version
 
 
 def load_unittest(test_module, test_class, test_name, options):
@@ -278,13 +287,25 @@ class DistributionMgr(threading.Thread):
                 workers.append({
                     "host": host,
                     "password": test.conf_get(host, 'password', ''),
-                    "username": test.conf_get(host, 'username', '')
-                })
+                    "username": test.conf_get(host, 'username', '')})
 
         self._workers = []
         [self._workers.append(SSHDistributor(**w)) for w in workers]
         self._worker_results = {}
         trace(str(self))
+
+        # setup monitoring
+        monitor_hosts = []                  # list of (host, port, descr)
+        if not options.is_distributed:
+            hosts = test.conf_get('monitor', 'hosts', '', quiet=True).split()
+            for host in sorted(hosts):
+                host = host.strip()
+                monitor_hosts.append((host, test.conf_getInt(host, 'port'),
+                                      test.conf_get(host, 'description', '')))
+        self.monitor_hosts = monitor_hosts
+        # keep the test to use the result logger for monitoring
+        # and call setUp/tearDown Cycle
+        self.test = test
 
     def __repr__(self):
         """Display distributed bench information."""
@@ -404,6 +425,8 @@ class DistributionMgr(threading.Thread):
         """
         threads = []
         trace("* Starting %d workers" % len(self._workers))
+
+        self.startMonitors()
         for worker in self._workers:
             venv = os.path.join(self.remote_res_dir, self.tarred_testsdir)
             obj = worker.threaded_execute(
@@ -424,6 +447,12 @@ class DistributionMgr(threading.Thread):
                         in err_string.split("\n") if k.strip()))
             trace("\n")
 
+        self.stopMonitors()
+        self.logr_close()
+
+        self.stopMonitors()
+        self.correlate_statistics()
+
     def final_collect(self):
         expr = re.compile("Log\s+xml:\s+(.*?)\n")
         for worker, results in self._worker_results.items():
@@ -433,7 +462,148 @@ class DistributionMgr(threading.Thread):
                 filename = os.path.split(remote_file)[1]
                 local_file = os.path.join(
                     self.distribution_output, "%s-%s" % (
-                        worker.host , filename))
+                        worker.host, filename))
                 worker.get(remote_file, local_file)
                 trace("* Received bench log from [%s] into %s\n" % (
                     worker.host, local_file))
+
+    def startMonitors(self):
+        """Start monitoring on hosts list."""
+        if not self.monitor_hosts:
+            return
+        monitor_hosts = []
+        monitor_key = "%s:0:0" % self.method_name
+        for (host, port, desc) in self.monitor_hosts:
+            trace("* Start monitoring %s: ..." % host)
+            server = ServerProxy("http://%s:%s" % (host, port))
+            try:
+                server.startRecord(monitor_key)
+            except SocketError:
+                trace(' failed, server is down.\n')
+            else:
+                trace(' done.\n')
+                monitor_hosts.append((host, port, desc))
+        self.monitor_hosts = monitor_hosts
+
+    def stopMonitors(self):
+        """Stop monitoring and save xml result."""
+        if not self.monitor_hosts:
+            return
+        monitor_key = "%s:0:0" % self.method_name
+        successful_results = []
+        for (host, port, desc) in self.monitor_hosts:
+            trace('* Stop monitoring %s: ' % host)
+            server = ServerProxy("http://%s:%s" % (host, port))
+            try:
+                server.stopRecord(monitor_key)
+                successful_results.append(server.getXmlResult(monitor_key))
+            except SocketError:
+                trace(' failed, server is down.\n')
+            else:
+                trace(' done.\n')
+
+        self.write_statistics(successful_results)
+
+    def write_statistics(self, successful_results):
+        """ Write the distributed stats to a file in the output dir """
+        path = os.path.join(self.distribution_output, "stats.xml")
+        config = {'id': self.test_id,
+                  'description': self.test_description,
+                  'class_title': self.class_title,
+                  'class_description': self.class_description,
+                  'module': self.module_name,
+                  'class': self.class_name,
+                  'method': self.method_name,
+                  'cycles': self.cycles,
+                  'duration': self.duration,
+                  'sleep_time': self.sleep_time,
+                  'startup_delay': self.startup_delay,
+                  'sleep_time_min': self.sleep_time_min,
+                  'sleep_time_max': self.sleep_time_max,
+                  'cycle_time': self.cycle_time,
+                  'configuration_file': self.config_path,
+                  'server_url': self.test_url,
+                  'log_xml': self.result_path,
+                  'python_version': platform.python_version()}
+
+        for (host, port, desc) in self.monitor_hosts:
+            config[host] = desc
+
+        with open(path, "w+") as fd:
+            fd.write('<funkload version="{version}" time="{time}">\n'.format(
+                            version=get_version(), time=time.time()))
+            for key, value in config.items():
+                # Write out the config values
+                fd.write('<config key="{key}" value="{value}"/>\n'.format(
+                                                        key=key, value=value))
+            for xml in successful_results:
+                fd.write(xml)
+                fd.write("\n")
+
+            fd.write("</funkload>\n")
+
+    def _calculate_time_skew(self, results, stats):
+        def min_time(vals):
+            keyfunc = lambda elem: float(elem.attrib['time'])
+            return keyfunc(min(vals, key=keyfunc))
+
+        results_min = min_time(results)
+        monitor_min = min_time(stats)
+
+        return results_min / monitor_min
+
+    def _calculate_results_ranges(self, results):
+        seen = []
+        times = {}
+        for element in results:
+            cycle = int(element.attrib['cycle'])
+            if cycle not in seen:
+                seen.append(cycle)
+
+                cvus = int(element.attrib['cvus'])
+                start_time = float(element.attrib['time'])
+                times[start_time] = (cycle, cvus)
+
+        return times
+
+    def correlate_statistics(self):
+        result_path = None
+        for worker, results in self._worker_results.items():
+            files = glob("%s/%s-*.xml" % (self.distribution_output,
+                                          worker.host))
+            if files:
+                result_path = files[0]
+                break
+
+        if not result_path:
+            trace("* No output files found; unable to correlate stats.\n")
+            return
+
+        # Calculate the ratio between results and monitoring
+        results_tree = ElementTree(file=result_path)
+        stats_path = os.path.join(self.distribution_output, "stats.xml")
+        stats_tree = ElementTree(file=stats_path)
+
+        results = results_tree.findall("testResult")
+        stats = stats_tree.findall("monitor")
+        ratio = self._calculate_time_skew(results, stats)
+
+        # Now that we have the ratio, we can calculate the sessions!
+        times = self._calculate_results_ranges(results)
+        times_desc = sorted(times.keys(), reverse=True)
+
+        # Now, parse the stats tree and update values
+        def find_range(start_time):
+            for time_ in times_desc:
+                if start_time > time_:
+                    return times[time_]
+            else:
+                return times[time_]
+
+        for stat in stats:
+            adj_time = float(stat.attrib['time']) * ratio
+            cycle, cvus = find_range(adj_time)
+            key, cycle_, cvus_ = stat.attrib['key'].partition(':')
+            stat.attrib['key'] = "%s:%d:%d" % (key, cycle, cvus)
+
+        stats_tree.write(stats_path)
