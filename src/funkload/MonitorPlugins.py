28998bf9e168d01ead01bf53ffd6608ab2215af1
bdelbosc <bdelbosc@nuxeo.com>
2012-03-13 12:05:55 +0100

    Pep8

--- src/funkload/MonitorPlugins.py
+++ src/funkload/MonitorPlugins.py
@@ -15,36 +15,42 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 # 02111-1307, USA.
 #
-import pkg_resources, re, pickle, sys
+import sys
+import re
+import pickle
+import pkg_resources
+
 ENTRYPOINT = 'funkload.plugins.monitor'
 
-gd_colors=[['red', 0xff0000],
-           ['green', 0x00ff00],
-           ['blue', 0x0000ff],
-           ['yellow', 0xffff00],
-           ['purple', 0x7f007f],
-          ] 
+gd_colors = [['red', 0xff0000],
+             ['green', 0x00ff00],
+             ['blue', 0x0000ff],
+             ['yellow', 0xffff00],
+             ['purple', 0x7f007f],
+             ]
+
 
 class MonitorPlugins():
     MONITORS = {}
+
     def __init__(self, conf=None):
         self.conf = conf
-        self.enabled=None
-        self.disabled=None
-        if conf==None or not conf.has_section('plugins'):
+        self.enabled = None
+        self.disabled = None
+        if conf == None or not conf.has_section('plugins'):
             return
         if conf.has_option('plugins', 'monitors_enabled'):
-            self.enabled=re.split(r'\s+', conf.get('plugins', 'monitors_enabled'))
+            self.enabled = re.split(r'\s+', conf.get('plugins', 'monitors_enabled'))
         if conf.has_option('plugins', 'monitors_disabled'):
-            self.disabled=re.split(r'\s+', conf.get('plugins', 'monitors_disabled'))
+            self.disabled = re.split(r'\s+', conf.get('plugins', 'monitors_disabled'))
 
     def registerPlugins(self):
         for entrypoint in pkg_resources.iter_entry_points(ENTRYPOINT):
             p = entrypoint.load()(self.conf)
-            if self.enabled!=None:
+            if self.enabled != None:
                 if p.name in self.enabled:
                     self.MONITORS[p.name] = p
-            elif self.disabled!=None:
+            elif self.disabled != None:
                 if p.name not in self.disabled:
                     self.MONITORS[p.name] = p
             else:
@@ -55,21 +61,23 @@ class MonitorPlugins():
             if config.has_key(plugin.name):
                 plugin.setConfig(config[plugin.name])
 
+
 class Plot:
     def __init__(self, plots, title="", ylabel="", unit="", **kwargs):
-        self.plots=plots
-        self.title=title
-        self.ylabel=ylabel
-        self.unit=unit
+        self.plots = plots
+        self.title = title
+        self.ylabel = ylabel
+        self.unit = unit
         for key in kwargs:
             setattr(self, key, kwargs[key])
+
 
 class MonitorPlugin(object):
     def __init__(self, conf=None):
         if not hasattr(self, 'name') or self.name == None:
-            self.name=self.__class__.__name__
+            self.name = self.__class__.__name__
         if not hasattr(self, 'plots'):
-            self.plots=[]
+            self.plots = []
         self._conf = conf
 
     def _checkKernelRev(self):
@@ -84,38 +92,38 @@ class MonitorPlugin(object):
         return kernel_rev
 
     def gnuplot(self, times, host, image_prefix, data_prefix, gplot_path, chart_size, stats):
-        parsed=self.parseStats(stats)
-        if parsed==None:
+        parsed = self.parseStats(stats)
+        if parsed == None:
             return None
 
-        image_path="%s.png" % image_prefix
-        data_path="%s.data" % data_prefix
+        image_path = "%s.png" % image_prefix
+        data_path = "%s.data" % data_prefix
 
         data = [times]
         labels = ["TIME"]
         plotlines = []
         plotsno = 0
         for plot in self.plots:
-            if len(plot.plots)==0:
+            if len(plot.plots) == 0:
                 continue
             ylabel = plot.ylabel
-            if plot.unit!="":
-                ylabel+='[%s]' % plot.unit
+            if plot.unit != "":
+                ylabel += '[%s]' % plot.unit
             plotlines.append('set title "%s"' % plot.title)
             plotlines.append('set ylabel "%s"' % ylabel)
             plot_line = 'plot "%s"' % data_path
 
-            li=[]
+            li = []
             for p in plot.plots.keys():
                 data.append(parsed[p])
                 labels.append(p)
                 li.append(' u 1:%d title "%s" with %s' % (len(data), plot.plots[p][1], plot.plots[p][0]))
-            plotlines.append(plot_line+', ""'.join(li))
-            plotsno+=1
-        
-        lines=[]
+            plotlines.append(plot_line + ', ""'.join(li))
+            plotsno += 1
+
+        lines = []
         lines.append('set output "%s"' % image_path)
-        lines.append('set terminal png size %d,%d' % (chart_size[0], chart_size[1]*plotsno))
+        lines.append('set terminal png size %d,%d' % (chart_size[0], chart_size[1] * plotsno))
         lines.append('set grid back')
         lines.append('set xdata time')
         lines.append('set timefmt "%H:%M:%S"')
@@ -137,26 +145,26 @@ class MonitorPlugin(object):
         return [(self.name, image_path)]
 
     def gdchart(self, x, times, host, image_prefix, stats):
-        parsed=self.parseStats(stats)
-        if parsed==None:
+        parsed = self.parseStats(stats)
+        if parsed == None:
             return None
 
         ret = []
-        i=0
+        i = 0
         for plot in self.plots:
-            image_path="%s_%d.png" % (image_prefix, i)
-            i+=1
-            title="%s:"%host
-            data=[]
-            title_parts=[]
-            j=0
+            image_path = "%s_%d.png" % (image_prefix, i)
+            i += 1
+            title = "%s:" % host
+            data = []
+            title_parts = []
+            j = 0
             for p in plot.plots.keys():
                 data.append(parsed[p])
-                title_parts.append(" %s (%s)"%(plot.plots[p][1], gd_colors[j][0]))
-                j+=1
-            title+=", ".join(title_parts)
+                title_parts.append(" %s (%s)" % (plot.plots[p][1], gd_colors[j][0]))
+                j += 1
+            title += ", ".join(title_parts)
 
-            colors=[]
+            colors = []
             for c in gd_colors:
                 colors.append(c[1])
 
@@ -183,7 +191,7 @@ class MonitorPlugin(object):
     def getStat(self):
         """ Read stats from system """
         pass
-  
+
     def parseStats(self, stats):
         """ Parse MonitorInfo object list """
         pass
